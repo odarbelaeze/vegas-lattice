87aab4d292eddc8d4670ab5b33446551f62edc90
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2016-01-11 08:51:08 -0500

    Adding setup building

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='vegas-lattice',
+    version='0.1.0',
+    author='Oscar D. Arbeláez <@odarbelaeze>, Juan D. Alzate <@jdalzatec>',
+    author_email='odarbelaeze@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.9.2',
+        'click>=6.2',
+    ],
+    entry_points='''
+    [console_scripts]
+    vegas-lattice=vlatice.scripts:cli
+    ''',
+    url='https://github.com/odarbelaeze/vegas-lattice',
+    download_url='https://github.com/odarbelaeze/vegas-lattice/tarball/0.1.0',
+    keywords=['lattice', 'graph', 'magnetite', 'construction', ],
+    description='A magnetite sample generator',
+)
