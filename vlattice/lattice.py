09bd2d9022ccf2c11b3071d67669c43eb8e109a7
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2016-01-17 21:13:05 -0500

    Adding lattice depletion.

--- vlattice/lattice.py
+++ vlattice/lattice.py
@@ -10,7 +10,9 @@ import operator
 import numpy
 
 
-Interaction = collections.namedtuple('Interaction', ['source', 'target', 'exchange'])
+Interaction = collections.namedtuple(
+    'Interaction', ['source', 'target', 'exchange']
+)
 
 
 class Atom(object):
@@ -65,6 +67,21 @@ class Vertex(object):
         return True
 
 
+class Locator(object):
+    '''
+    A locator object that do coordinate system transforms
+    '''
+    def __init__(self, unitcell, crystal_coords=True):
+        self.unitcell = unitcell
+        self.crystal_coords = crystal_coords
+
+    def locate(self, site):
+        if self.crystal_coords:
+            return self.unitcell.dot(
+                numpy.array(site.coords) + site.atom.coords)
+        return self.unitcell.dot(site.coords) + site.atom.coords
+
+
 class Lattice(object):
     '''
     Represents a lattice with shape and periodicity
@@ -90,24 +107,24 @@ class Lattice(object):
             return False
         return True
 
-    def bring_in(self, site):
+    def bring_in(self, site, check_contains=True):
         '''
         Brings a site into the first replica of the lattice
         '''
-        if not self.contains(site):
+        if check_contains and not self.contains(site):
             raise ValueError('Site not contained in the lattice')
         pbc, coo, shape = self.pbc, site.coords, self.shape
         return Site(
             list(c % l if p else c for p, c, l in zip(pbc, coo, shape)),
             atom=site.atom)
 
-    def index(self, site):
+    def index(self, site, check_contains=True):
         '''
         Returns the index of a point in pbc
         '''
-        if not self.contains(site):
+        if check_contains and not self.contains(site):
             raise ValueError('Site not contained in the lattice')
-        coords = self.bring_in(site).coords
+        coords = self.bring_in(site, check_contains=check_contains).coords
         weights = list(itertools.accumulate(self.shape, operator.mul))
         weights = [1] + weights[:-1]
         return site.atom.id + self.natoms * sum(
@@ -135,21 +152,6 @@ class Lattice(object):
                 yield Interaction(site, tgt, vertex)
 
 
-class Locator(object):
-    '''
-    A locator object that do coordinate system transforms
-    '''
-    def __init__(self, unitcell, crystal_coords=True):
-        self.unitcell = unitcell
-        self.crystal_coords = crystal_coords
-
-    def locate(self, site):
-        if self.crystal_coords:
-            return self.unitcell.dot(
-                numpy.array(site.coords) + site.atom.coords)
-        return self.unitcell.dot(site.coords) + site.atom.coords
-
-
 class NanoParticle(Lattice):
     '''
     A spherical nanoparticle
@@ -166,6 +168,29 @@ class NanoParticle(Lattice):
     def contains(self, site):
         return super().contains(site) and numpy.linalg.norm(
             self.locator.locate(site) - self.centroid) <= self.radius
+
+    def sites(self):
+        for site in super().sites():
+            if self.contains(site):
+                yield site
+
+
+class DepletedLattice(Lattice):
+    '''
+    A randomly depleted lattice
+    '''
+    def __init__(self, probability, *args):
+        super().__init__(*args)
+        self.probability = probability
+        self._cache = {}
+
+    def contains(self, site):
+        if not super().contains(site):
+            return False
+        index = self.index(site, check_contains=False)
+        if index not in self._cache:
+            self._cache[index] = numpy.random.random() < self.probability
+        return self._cache[index]
 
     def sites(self):
         for site in super().sites():
